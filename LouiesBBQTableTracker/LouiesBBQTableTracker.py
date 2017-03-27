ad2dcaba1e21c7a4202b8ef326d3a882d0926c8e
Rick <Rick@WEMBLEY>
2017-03-27 12:08:35 -0400

    Add project files.

---
+++ LouiesBBQTableTracker/LouiesBBQTableTracker.py
@@ -0,0 +1,8 @@
+#Project: CIS 177 WEEK 8 PROJECT
+#Project Location: projects\cis177\dateConvert2
+#File: dateConvert2.py
+#Purpose: Accept a user-entered numeric form date (07/14/1977),
+#         then convert the entry to the long form date (14-July-1977)
+#Revision: 1.0 / 12 MAR 2017
+#Created: 12 MAR 2017
+#Author: Rick Miller <rick@rickthegeek.com>
